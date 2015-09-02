dc90a174c8cd835794347f4291098bbba8280a7c
Aleksey Blinov <a6a6un@yahoo.com>
2015-09-02 13:58:31 -0500

    Minor code folding in bounds constraint checking function.

--- constraint.go
+++ constraint.go
@@ -50,10 +50,10 @@ func checkBounds(d interface{}, t metadata, bg boundaryGetter) error {
 		lm := s.MethodByName("Less"); if lm == z { lm = s.MethodByName("Before"); }
 		gm := s.MethodByName("Greater"); if gm == z { gm = s.MethodByName("After"); }
 		if lm != z && gm != z { // via explicit methods for ordering detection
-			if min != nil { ls = fmt.Sprintf("%v", min.Interface()); }
-			if max != nil { us = fmt.Sprintf("%v", max.Interface()); }
 			obl = min != nil && lm.Call([]reflect.Value {min.Elem()})[0].Bool()
 			obh = max != nil && gm.Call([]reflect.Value {max.Elem()})[0].Bool()
+			if min != nil { ls = fmt.Sprintf("%v", min.Interface()); }
+			if max != nil { us = fmt.Sprintf("%v", max.Interface()); }
 			vs = fmt.Sprintf("%v", s.Interface())
 		} else { // via kind ordering
 			rv := reflect.ValueOf(d)
@@ -65,31 +65,25 @@ func checkBounds(d interface{}, t metadata, bg boundaryGetter) error {
 				if min != nil { v := min.Elem(); min = &v; }
 				if max != nil { v := max.Elem(); max = &v; }
 			}
+			if min != nil { ls = fmt.Sprintf("%v", min.Interface()); }
+			if max != nil { us = fmt.Sprintf("%v", max.Interface()); }
+			vs = fmt.Sprintf("%v", v)
 			switch vk {
 			case reflect.String:
-				if min != nil { ls = min.String(); }
-				if max != nil { us = max.String(); }
-				obl = min != nil && rv.String() < ls
-				obh = max != nil && rv.String() > us
-				vs = fmt.Sprintf("\"%s\"", rv.String())
+				if min != nil { ls = fmt.Sprintf("\"%s\"", ls); }
+				if max != nil { us = fmt.Sprintf("\"%s\"", us); }
+				obl = min != nil && rv.String() < min.String()
+				obh = max != nil && rv.String() > max.String()
+				vs = fmt.Sprintf("\"%s\"", vs)
 			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-				if min != nil { ls = fmt.Sprintf("%v", min.Interface()); }
-				if max != nil { us = fmt.Sprintf("%v", max.Interface()); }
 				obl = min != nil && rv.Int() < min.Int()
 				obh = max != nil && rv.Int() > max.Int()
-				vs = fmt.Sprintf("%v", v)
 			case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-				if min != nil { ls = fmt.Sprintf("%v", min.Interface()); }
-				if max != nil { us = fmt.Sprintf("%v", max.Interface()); }
 				obl = min != nil && rv.Uint() < min.Uint()
 				obh = max != nil && rv.Uint() > max.Uint()
-				vs = fmt.Sprintf("%v", v)
 			case reflect.Float32, reflect.Float64:
-				if min != nil { ls = fmt.Sprintf("%v", min.Interface()); }
-				if max != nil { us = fmt.Sprintf("%v", max.Interface()); }
 				obl = min != nil && rv.Float() < min.Float()
 				obh = max != nil && rv.Float() > max.Float()
-				vs = fmt.Sprintf("%v", v)
 			}
 		}
 	}
